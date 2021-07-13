eee902f724231146824165a5a454341f15d02d2c
Mishkat Mustafid <mishkatmustafid@gmail.com>
2021-07-13 14:01:14 +0600

    widgets ready to be modified

---
+++ arektu_shikhi/lib/itemview.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Itemview extends StatefulWidget {
+  const Itemview({ Key? key }) : super(key: key);
+
+  @override
+  _ItemviewState createState() => _ItemviewState();
+}
+
+class _ItemviewState extends State<Itemview> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
