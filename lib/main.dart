7709954c75fcdb46240680c3ac82bca360b9f995
Mishkat Mustafid <mishkatmustafid@gmail.com>
2021-09-09 18:57:46 +0600

    firebase initialized at main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:arektu_shikhi/routes.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]);
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  var fsconnect = FirebaseFirestore.instance;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
