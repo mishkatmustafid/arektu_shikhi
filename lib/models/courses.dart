0147b71c54ed1ab227b7aa0da3140ab55f852b83
Mishkat Mustafid <mishkatmustafid@gmail.com>
2021-09-15 12:19:46 +0600

    models updated

--- lib/models/courses.dart
+++ lib/models/courses.dart
@@ -1,25 +1,4 @@
 class Course {
-  String title =" ", link =" ";
-
-  Course(String title, String link){
-    this.title = title;
-    this.link = link;
-  }
+  final String title, link;
+  Course({this.title="", this.link=""});
 }
-
-List<Course> courses = [
-  Course(
-    "Dart for Android Developers", 
-    "https://www.youtube.com/watch?v=wHXl4HeuwiM",
-  ),
-
-  Course(
-    "Android for Beginners", 
-    "https://www.youtube.com/watch?v=CRZPkwmoOpM",
-  ),
-
-  Course(
-    "Android for Beginners", 
-    "https://www.youtube.com/watch?v=CRZPkwmoOpM",
-  ),
-];
--- lib/models/users.dart
+++ lib/models/users.dart
@@ -1,16 +1,10 @@
 class User {
-  String 
-  uid = "",
-  username = "",
-  fullname = "",
-  phone = "", 
-  grade = "";
+  String uid, username, fullname, phone, grade;
 
-  User(String uid, String username, String fullname, String phone, String grade) {
-    this.uid = uid;
-    this.username = username;
-    this.fullname = fullname;
-    this.phone = phone;
-    this.grade = grade;
-  }
+  User(
+      {this.uid = "",
+      this.username = "",
+      this.fullname = "",
+      this.phone = "",
+      this.grade = ""});
 }
