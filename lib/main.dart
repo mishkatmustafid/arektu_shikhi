e57a27f41d49cec089de497c268a85c051c679bb
Mishkat Mustafid <mishkatmustafid@gmail.com>
2021-09-19 12:49:48 +0600

    providers added

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:arektu_shikhi/routes.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,14 +16,17 @@ class MyApp extends StatelessWidget {
   var fsconnect = FirebaseFirestore.instance;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Arektu Shikhi',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [],
+      child: MaterialApp(
+        title: 'Arektu Shikhi',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: '/signup',
+        routes: routes,
       ),
-      initialRoute: '/signup',
-      routes: routes,
     );
   }
 }
